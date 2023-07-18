9beb4ee0de6e2c4dd32add6d18f61314ca8230b7
RobbyTato <ebiljacob99@gmail.com>
2023-07-18 19:41:42 +0300

    Moved boards into Bot class

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@
 
 class Bot:
 
-    def __init__(self, board):
-        self.board = board
-        
+    def __init__(self):
+        self.ship_board = [["-" for _ in range(10)] for _ in range(10)]
+        self.shot_board = [["-" for _ in range(10)] for _ in range(10)]
+
 
     def find_next_move(self, board):
         
