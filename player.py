aa458c508fcb291d8ad20bc88e9b97b4af9b8ec2
RobbyTato <ebiljacob99@gmail.com>
2023-07-19 14:26:49 +0300

    Finished setup_ships()

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import logging
 import keyboard
 from colorama import Fore, Back, Style
 import print_screen as ps
+
 logging.basicConfig(level=logging.DEBUG, filename="logs.txt", filemode="w", format="%(message)s")
 
 
@@ -11,35 +12,54 @@ class Player:
         self.shot_board = [["-" for _ in range(10)] for _ in range(10)]
 
     def setup_ships(self):
+
+        def refresh_ship_pos():
+            nonlocal ship_offset, ship_pos, extra, placeable
+            placeable = True
+            ship_offset = []
+            for i in range(ships[ship]):
+                x_offset = (i - (ships[ship] // 2)) * rotations[current_rotation][0]
+                y_offset = (i - (ships[ship] // 2)) * rotations[current_rotation][1]
+                ship_offset.append([x_offset, y_offset])
+            ship_pos = []
+            for i in ship_offset:
+                x = i[0] + cursor[0]
+                y = i[1] + cursor[1]
+                ship_pos.append([x, y])
+            outside = False  # check if ship is outside the box
+            for x, y in ship_pos:
+                if not (0 <= x <= 9 and 0 <= y <= 9):
+                    outside = True
+                    placeable = False
+                    break
+            extra = []
+            for x, y in ship_pos:
+                if not (0 <= x <= 9 and 0 <= y <= 9):
+                    placeable = False
+                elif not self.ship_board[x][y] == "-":
+                    placeable = False
+                    extra.append([[x, y], Back.RED])
+                else:
+                    if outside:
+                        extra.append([[x, y], Back.RED])
+                    else:
+                        extra.append([[x, y], Back.LIGHTBLACK_EX])
+
         ships = {"c": 5, "b": 4, "d": 3, "s": 3, "p": 2}
+        ship_offset = []
+        ship_pos = []
+        extra = []
+        placeable = True
         rotations = ((1, 0), (0, 1))
         current_rotation = 0
         for ship in ships:
             cursor = [0, 0]
+            refresh_ship_pos()
+            self.display_ship_board(extra=extra)
             while True:
-                placeable = True
-                ship_offset = []
-                for i in range(ships[ship]):
-                    x_offset = (i - (ships[ship] // 2)) * rotations[current_rotation][0]
-                    y_offset = (i - (ships[ship] // 2)) * rotations[current_rotation][1]
-                    ship_offset.append([x_offset, y_offset])
-                ship_pos = []
-                for i in ship_offset:
-                    x = i[0] + cursor[0]
-                    y = i[1] + cursor[1]
-                    if not (0 <= x <= 9 and 0 <= y <= 9):
-                        placeable = False
-                    if not self.ship_board[x][y] == "-":
-                        placeable = False
-                        ship_pos.append([[x, y], Back.RED])
-                    else:
-                        ship_pos.append([[x, y], Back.LIGHTBLACK_EX])
-                logging.debug(ship_pos)
-                self.display_ship_board(extra=ship_pos)
-
                 key = keyboard.read_event()
                 if key.event_type == "up" or key.name not in ("up", "down", "left", "right",
-                                                               "w", "s", "a", "d", "enter", "r"):
+                                                              "w", "s", "a", "d", "enter", "r"):
                     continue
                 if key.name in ("up", "w"):
                     if cursor[0] != 0:
@@ -69,35 +89,37 @@ class Player:
                 if key.name == "enter":
                     if placeable:
                         for i in ship_pos:
-                            self.ship_board[i[0][0]][i[0][1]] = ship
+                            self.ship_board[i[0]][i[1]] = ship
                         break
                     else:
                         continue
 
-            ship_offset = []
+                refresh_ship_pos()
+                self.display_ship_board(extra=extra)
 
     def ship_board_to_string(self, extra=None):
-        s = "╔" + ("═" * 20) + "╗\n"  # first row
+        s = "╔" + ("═" * 40) + "╗\n"  # first row
         for x in range(10):  # add the board
-            s += "║"
+            line = "║"
             for y in range(10):
                 if extra is None:
                     if self.ship_board[x][y] == "-":
-                        s += "  "
+                        line += Back.LIGHTBLUE_EX + "    " + Style.RESET_ALL
                     else:
-                        s += Back.LIGHTBLACK_EX + "  " + Style.RESET_ALL
+                        line += Back.LIGHTBLACK_EX + "    " + Style.RESET_ALL
                 else:
                     for i in extra:
                         if i[0] == [x, y]:
-                            s += i[1] + "  " + Style.RESET_ALL
+                            line += i[1] + "    " + Style.RESET_ALL
                             break
                     else:
                         if self.ship_board[x][y] == "-":
-                            s += "  "
+                            line += Back.LIGHTBLUE_EX + "    " + Style.RESET_ALL
                         else:
-                            s += Back.LIGHTBLACK_EX + "  " + Style.RESET_ALL
-            s += "║\n"
-        s += "╚" + ("═" * 20) + "╝"  # last row
+                            line += Back.LIGHTBLACK_EX + "    " + Style.RESET_ALL
+            line += "║\n"
+            s += line * 2
+        s += "╚" + ("═" * 40) + "╝"  # last row
         return s
 
     def display_ship_board(self, extra=None):
